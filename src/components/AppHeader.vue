e8cbf5408f7a08603784855f3706e1408d7b8645
stappost <emanuele.scarpantoni@gmail.com>
2024-01-10 16:18:01 +0100

    final project add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@ export default {
 <template lang="">
     <header>
         <div class="logo">
-
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="logo">
         </div>
         <h2>Yu-Gi-Oh Api</h2>
     </header>
@@ -14,9 +14,21 @@ export default {
 <style lang="scss" scoped>
 header {
     padding: 10px;
+    display: flex;
+    align-items: center;
+
+    .logo {
+        max-width: 100px;
+
+        img {
+            width: 100%;
+        }
+    }
 
     h2 {
         font-weight: 100;
+        font-size: 30px;
+        margin-left: 10px;
     }
 }
 </style>
