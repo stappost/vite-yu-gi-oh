f8bb12b8fd987e972d968493d9d35a8804662ad8
stappost <emanuele.scarpantoni@gmail.com>
2024-01-10 14:51:30 +0100

    add axios and header in App.vue without logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script>
-export default {
+import axios from "axios"
+import AppHeader from './components/AppHeader.vue';
+import { store } from './store';
 
-}
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getCards() {
+      axios.get(store.endpoint).then(results => {
+        store.cards = results.data.data
+      })
+    }
+  },
+  created() {
+    this.getCards()
+  }
+
+} 
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader />
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+@use './styles/generals.scss'
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,10 @@ export default {
 </script>
 <template lang="">
     <div>
-        
+        <div class="logo">
+
+        </div>
+        <h2>Yu-Gi-Oh Api</h2>
     </div>
 </template>
-<style lang="">
-    
-</style>
+<style lang="scss" scoped></style>
